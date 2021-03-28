34b59977c85b919a1adb88c01a694f73ebb2e3fe
TANMAYBAGADIA <tanmaybagadia1301@gmail.com>
2021-03-28 22:21:28 +0530

    Title Done

---
+++ src/pages/Create.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles()
+ 
+
+export default function Create() {
+  const classes = useStyles();
+
+  return (
+    <Container size="sm">
+      <Typography
+        className={classes.title}
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+
+      <Button
+        className={classes.btn}
+        onClick={() => console.log("you clicked me")}
+        type="submit"
+        color="secondary"
+        variant="contained"
+        endIcon={<KeyboardArrowRightIcon />}
+      >
+        Submit
+      </Button>
+    </Container>
+  );
+}
